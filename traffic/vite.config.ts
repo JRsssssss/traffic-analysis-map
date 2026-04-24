739d982c205fe9b89e8eaaad3ef94b680bbd4ecc
JRsssssss <tackelzaza@gmail.com>
2026-04-24 11:57:44 +0700

    update tanstack router

--- traffic/vite.config.ts
+++ traffic/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 // import { devtools } from '@tanstack/devtools-vite'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import {tanstackRouter} from '@tanstack/router-plugin/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [tailwindcss(), tanstackRouter(), viteReact()],
   base: process.env.VITE_BASE_PATH || "traffic-analysis-map",
 })
 
