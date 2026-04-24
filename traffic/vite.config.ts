38176f062727fef61a29b623da3450a8f78500a9
JRsssssss <tackelzaza@gmail.com>
2026-04-24 11:39:34 +0700

    update vite.config

--- traffic/vite.config.ts
+++ traffic/vite.config.ts
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [tailwindcss(), tanstackStart(), viteReact()],
+  base: process.env.VITE_BASE_PATH || "/traffic-analysis-map/tree/main/traffic",
 })
 
 export default config
